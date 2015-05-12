a0293ae6c1869eea5d7ec391b46eed9c89b14eff
lewicki-pk <lewicki.pk@gmail.com>
2015-05-12 23:49:16 +0200

    travis CI fix + setup/teardown support

--- UT/src/SitemapGeneratorTest.cpp
+++ UT/src/SitemapGeneratorTest.cpp
@@ -5,7 +5,14 @@
 #include <fstream>
 #include <sstream>
 
-TEST(SitemapGenerator, generateFullSitemapValidation) {
+class SitemapGeneratorTest : public ::testing::Test
+{
+protected:
+    void SetUp() {};
+    void TearDown() {};
+};
+
+TEST_F(SitemapGeneratorTest, generateFullSitemapValidation) {
     std::stringstream goldenSitemap;
     goldenSitemap << "sitemap thesisSystem label=\"Main Menu\" \n{\n}\n";
     SitemapGenerator* testableGenerator = new SitemapGenerator();
